dbc2880890d58b73cddc9f4681b72a02d3030d25
Drawcris <sirwoj2@wp.pl>
2025-11-18 15:38:25 +0100

    home

--- src/app/home/pages/home-page/home-page.ts
+++ src/app/home/pages/home-page/home-page.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import { MatIconModule } from "@angular/material/icon";
+import { A11yModule } from "@angular/cdk/a11y";
+import {MatTableModule} from '@angular/material/table';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [MatGridListModule, MatCardModule, MatIconModule, A11yModule, MatTableModule],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
 export class HomePage {
-
+ 
 }
